6d0160e054eba414173e99ecb94d37c21c97a4fb
Daniel Corcoran <danielc.92@outlook.com>
2019-06-21 10:21:03 +1000

    Registered models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Placement, Bid, PlacementBid, Company
 
 # Register your models here.
+admin.site.register(Placement)
+admin.site.register(PlacementBid)
+admin.site.register(Bid)
+admin.site.register(Company)
