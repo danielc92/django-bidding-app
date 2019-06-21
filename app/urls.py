2219813a3d3fc1a7c4b9a0553a032b1c727c2ef9
Daniel Corcoran <danielc.92@outlook.com>
2019-06-21 10:40:48 +1000

    Added urls and login view

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('home/', views.home, name='home'),
+]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import CustomRegisterForm
+from .models import Placement
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -14,3 +17,22 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            
+    else:
+        return render(request, 'login.html') 
+
+
+def logout(request):
+    logout(request)
