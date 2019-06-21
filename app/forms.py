8e08bbf5245ea2709f75a4052bd11a1bc567f20f
Daniel Corcoran <danielc.92@outlook.com>
2019-06-21 10:11:26 +1000

    Added custom register form

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomRegisterForm(UserCreationForm):
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
