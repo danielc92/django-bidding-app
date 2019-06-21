39cd4580d359a5a70c46d3bd0f47f012c2b2e710
Daniel Corcoran <danielc.92@outlook.com>
2019-06-21 10:19:30 +1000

    Made initial migration

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Company(models.Model):
     """
-    Simply contains company details
+    Simply contains company details, referenced by Placement model
     """
 
     company_name = models.CharField(max_length=255)
@@ -19,26 +19,11 @@ class Company(models.Model):
         return self.company_name
 
 
-class PlacementBid(models.Model):
-    """
-    The junction table for placement and bid models/tables. Contains every instance of a bid for a placement
-    """
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bid_amount = models.IntegerField()
-    bid_shares = models.IntegerField()
-    
-    placementbid_created = models.DateTimeField(auto_now_add=True)
-    placementbid_modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.
-
-
 class Placement(models.Model):
     """
     A placement allows investors to bid on company capital raise
     """
+
     placement_title = models.CharField(max_length=255)
     placement_slug = models.SlugField()
     placement_company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -48,11 +33,13 @@ class Placement(models.Model):
     placement_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.
+        return self.placement_title
 
 
 class Bid(models.Model):
-
+    """
+    The bid, synonmous with 'order'
+    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bid_status = models.BooleanField(default=False)
 
@@ -60,4 +47,23 @@ class Bid(models.Model):
     bid_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user
+        return '{} -{}'.format(self.user, self.bid_status)
+
+
+class PlacementBid(models.Model):
+    """
+    The junction table for placement and bid models/tables. Contains every instance of a bid for a placement
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    placement = models.ForeignKey(Placement, on_delete=models.CASCADE)
+    bid  = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    offer = models.IntegerField()
+    shares = models.IntegerField()
+    confirmed = models.BooleanField(default=False)
+    
+    placementbid_created = models.DateTimeField(auto_now_add=True)
+    placementbid_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.shares, self.offer, self.user)
