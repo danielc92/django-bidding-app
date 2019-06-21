9fdadb02378da08dcdf8ea95b6803adec21ec6a2
Daniel Corcoran <danielc.92@outlook.com>
2019-06-21 10:30:18 +1000

    Add register view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import CustomRegisterForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app:home')
+    else:
+        form = CustomRegisterForm()
+    
+    context = {'form': form}
+
+    return render(request, 'register.html', context)
