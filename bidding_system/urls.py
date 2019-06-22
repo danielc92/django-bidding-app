12af03be6dcfa16928a2969e69456ea0059a44a8
Daniel Corcoran <danielc.92@outlook.com>
2019-06-22 10:08:45 +1000

    Finished About page

--- bidding_system/urls.py
+++ bidding_system/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('app.urls', namespace='app'))
 ]
